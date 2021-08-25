aeae041f72aaa54dc030f1d44d69d5e245ea2273
takanobu <takanb079@gmail.com>
2021-08-25 16:20:31 +0900

    Q11~Q16を回答

--- task.rb
+++ task.rb
@@ -111,6 +111,8 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  sports.flatten!.uniq!
+  sports.each.with_index(1) {|sport,i| puts"No#{i} #{sport}"}
 
 end
 
@@ -118,6 +120,7 @@ def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
+  puts data[:user][:name]
 
 end
 
@@ -126,6 +129,8 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
+  user_data.update(update_data)
+  puts user_data
 
 end
 
@@ -133,6 +138,8 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
+  key_array = data.keys
+  p key_array
 
 end
 
@@ -141,6 +148,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
+  puts data1[:age] ? "OK" : "NG"
+  puts data2[:age] ? "OK" : "NG"
 
 end
 
@@ -153,7 +162,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}です。"
+  end
 end
 
 class UserQ17
