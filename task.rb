f74d8165bba56446e4d7068e754ad035fd863f9d
takanobu <takanb079@gmail.com>
2021-09-23 10:52:38 +0900

    Q17~Q20を回答

--- task.rb
+++ task.rb
@@ -169,7 +169,18 @@ end
 
 class UserQ17
   # 以下に回答を記載
-
+  def initialize(name:, age:, gender:)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+  def info
+    puts <<~TEXT
+      名前：#{@name}
+      年齢：#{@age}
+      性別：#{@gender}
+    TEXT
+  end
 end
 
 def q17
@@ -184,7 +195,17 @@ end
 
 class UserQ18
   # 以下に回答を記載
-
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
+  def introduce
+    if @age >= 20
+      "こんにちは，#{@name}と申します。宜しくお願いいたします。"
+    else
+      "はいさいまいど〜，#{@name}です！！！"
+    end
+  end
 end
 
 def q18
@@ -198,8 +219,8 @@ end
 
 class Item
   # 以下を修正して下さい
-
-  def initialize(name)
+  attr_accessor :name
+  def initialize(name:)
     @name = name
   end
 end
@@ -212,11 +233,31 @@ end
 
 class UserQ20
   # 以下に回答を記載
-
+  attr_accessor :name, :age
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
 end
 
 class Zoo
   # 以下に回答を記載
+  def initialize (name:, entry_fee:)
+    @zoo_name = name
+    @entry_fee = entry_fee
+  end
+  def info_entry_fee(user)
+    case user.age
+    when 0..5
+      puts "#{user.name}さんの入場料金は #{@entry_fee[:infant]} 円です。"
+    when 6..12
+      puts "#{user.name}さんの入場料金は #{@entry_fee[:children]} 円です。"
+    when 13..64
+      puts "#{user.name}さんの入場料金は #{@entry_fee[:adult]} 円です。"
+    when 65..120
+      puts "#{user.name}さんの入場料金は #{@entry_fee[:senior]} 円です。"
+    end
+  end
 
 end
 
